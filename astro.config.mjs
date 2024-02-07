2fad3ce5ac8710ae4671b236fa0433ad653e463a
이원민 <wonmin@k60.dev>
2024-02-07 12:11:11 +0900

    chore: 검색엔진 노출을 위한 sitemap 경로 수정

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://powerjade.vercel.app",
   image: {
     service: squooshImageService(),
   },
