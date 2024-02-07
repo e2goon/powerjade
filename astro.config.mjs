231f34e23390bd46dda91d8d2c6e9f6e0e302c1c
이원민 <wonmin@k60.dev>
2024-02-07 12:21:59 +0900

    feat: `@vercel/analytics` 적용

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig, squooshImageService } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://powerjade.vercel.app",
   image: {
-    service: squooshImageService(),
+    service: squooshImageService()
   },
-  integrations: [sitemap(), tailwind()],
+  integrations: [sitemap(), tailwind(), react()]
 });
